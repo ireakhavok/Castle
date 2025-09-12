ec1b392e38c3690f716b5eeafe31441c6beddced
ireakhavok <ireakhavok@hotmail.com>
2025-09-11 21:46:43 -0500

    something 2?

--- Castle/SiegeEngine/UI/UIQuadRenderer.cs
+++ Castle/SiegeEngine/UI/UIQuadRenderer.cs
@@ -1,31 +1,29 @@
-﻿using System.Numerics;
+﻿// Folder: SiegeEngine.Rendering
+// File: UIQuadRenderer.cs
+using System.Numerics;
 using SiegeEngine.ContextManagement;
 using SiegeEngine.Rendering.Shaders;
+using System;
 
 namespace SiegeEngine.Rendering
 {
     public unsafe class UIQuadRenderer
     {
         private readonly IRenderContext _renderContext;
-        private uint _vao, _vbo;
+        private uint _vao, _vbo, _ebo;
         private ShaderProgram _shader;
-
         public UIQuadRenderer(IRenderContext renderContext)
         {
             _renderContext = renderContext;
             Initialize();
         }
-
         private void Initialize()
         {
             _shader = new ShaderProgram(_renderContext, UiShader.VertexSource, UiShader.FragmentSource);
-
             _renderContext.GenVertexArrays(1, out _vao);
             _renderContext.BindVertexArray(_vao);
-
             _renderContext.GenBuffers(1, out _vbo);
             _renderContext.BindBuffer(_renderContext.Enums.ArrayBuffer, _vbo);
-
             float[] vertices = new float[]
             {
                 0.0f, 0.0f,
@@ -33,18 +31,21 @@ namespace SiegeEngine.Rendering
                 1.0f, 1.0f,
                 0.0f, 1.0f
             };
-
             fixed (float* ptr = vertices)
             {
                 _renderContext.BufferData(_renderContext.Enums.ArrayBuffer, (uint)(vertices.Length * sizeof(float)), ptr, _renderContext.Enums.StaticDraw);
             }
-
             _renderContext.EnableVertexAttribArray(0);
             _renderContext.VertexAttribPointer(0, 2, _renderContext.Enums.Float, false, 2 * sizeof(float), (void*)0);
-
+            _renderContext.GenBuffers(1, out _ebo);
+            _renderContext.BindBuffer(_renderContext.Enums.ElementArrayBuffer, _ebo);
+            uint[] indices = new uint[] { 0, 1, 2, 0, 2, 3 };
+            fixed (uint* idxPtr = indices)
+            {
+                _renderContext.BufferData(_renderContext.Enums.ElementArrayBuffer, (uint)(indices.Length * sizeof(uint)), idxPtr, _renderContext.Enums.StaticDraw);
+            }
             _renderContext.BindVertexArray(0);
         }
-
         public void DrawQuad(Vector2 position, Vector2 size, Vector4 color, Matrix4x4 ortho)
         {
             _shader.Use();
@@ -54,9 +55,10 @@ namespace SiegeEngine.Rendering
             _shader.SetMatrix4("uTransform", ortho * model);
             _shader.SetUniform("uColor", color.X, color.Y, color.Z, color.W);
             _shader.SetUniform("uUseTexture", 0.0f);
-
             _renderContext.BindVertexArray(_vao);
-            _renderContext.DrawArrays(_renderContext.Enums.TriangleFan, 0, 4);
+            _renderContext.BindBuffer(_renderContext.Enums.ElementArrayBuffer, _ebo);
+            _renderContext.DrawElements(_renderContext.Enums.Triangles, 6, _renderContext.Enums.UnsignedInt, (void*)0);
+            _renderContext.BindBuffer(_renderContext.Enums.ElementArrayBuffer, 0);
             _renderContext.BindVertexArray(0);
         }
     }
