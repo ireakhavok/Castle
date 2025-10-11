8cbf417d6a0a72784a5c1c439db6edc1396057ec
ireakhavok <ireakhavok@hotmail.com>
2025-10-11 16:55:33 -0500

    menupanel start

---
+++ Castle/SiegeEngine/UI/MenuPanel.cs
@@ -0,0 +1,25 @@
+﻿// Folder: SiegeEngine.UI
+// File: MenuPanel.cs
+using SiegeEngine.ContextManagement;
+using SiegeEngine.Events;
+using SiegeEngine.Interfaces;
+using System;
+
+namespace SiegeEngine.UI
+{
+    public class MenuPanel : BasePanel
+    {
+        private string _htmlPath;
+
+        public MenuPanel(IRenderContext renderContext, IControlContext controlContext, IntPtr window, EventBus eventBus, string htmlPath) : base(renderContext, controlContext, window, eventBus)
+        {
+            _htmlPath = htmlPath;
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            //LoadUI(_htmlPath); //needs to be implemented
+        }
+    }
+}
