fe14486a9fd1c410c78138523479a559c7b3503f
ireakhavok <ireakhavok@hotmail.com>
2025-12-02 17:48:24 -0600

    FIXED!

--- Castle/SiegeEngine/UI/OptionElement.cs
+++ Castle/SiegeEngine/UI/OptionElement.cs
@@ -2,7 +2,8 @@
 // File: OptionElement.cs
 using System;
 using System.Numerics;
-
+using SiegeEngine.ContextManagement;
+using SiegeEngine.Rendering;
 namespace SiegeEngine.UI
 {
     public class OptionElement : HtmlElement
@@ -11,7 +12,6 @@ namespace SiegeEngine.UI
         {
             Tag = "option";
         }
-
         public override bool HandleClick(Vector2 mousePos, float viewportWidth, float viewportHeight)
         {
             var select = Parent as SelectElement;
@@ -20,6 +20,20 @@ namespace SiegeEngine.UI
                 return false; // Let the parent select handle the click to open
             }
             return base.HandleClick(mousePos, viewportWidth, viewportHeight);
+        }
+        public override void Render(IRenderContext renderContext, TextRenderer textRenderer, UIQuadRenderer quadRenderer, float viewportWidth, float viewportHeight, Matrix4x4 parentMatrix)
+        {
+            var select = Parent as SelectElement;
+            if (select != null && !select.IsOpen)
+            {
+                // Skip drawing background for displayed option in closed select
+                foreach (var child in Children)
+                {
+                    child.Render(renderContext, textRenderer, quadRenderer, viewportWidth, viewportHeight, parentMatrix);
+                }
+                return;
+            }
+            base.Render(renderContext, textRenderer, quadRenderer, viewportWidth, viewportHeight, parentMatrix);
         }
     }
 }
