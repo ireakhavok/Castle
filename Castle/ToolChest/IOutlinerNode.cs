01f25028f8aa2641a680bfe50b6988d358c31ad3
Joel Collins <ireakhavok@hotmail.com>
2026-04-09 22:32:06 -0500

    push before retry

---
+++ Castle/ToolChest/IOutlinerNode.cs
@@ -0,0 +1,17 @@
+﻿// Folder: ToolChest
+// File: IOutlinerNode.cs
+using System.Collections.Generic;
+using SiegeEngine.Core.UI;
+
+namespace ToolChest
+{
+    public interface IOutlinerNode
+    {
+        string Id { get; }
+        string Label { get; }
+        string Icon { get; }
+        IReadOnlyList<IOutlinerNode> Children { get; }
+        object BackingObject { get; }
+        HtmlElement ToHtmlElement(TreeViewPanel treeViewPanel);
+    }
+}
