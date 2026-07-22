5f3921e78b43af4b23b640cff3975feb20e2e67e
Joel Collins <ireakhavok@hotmail.com>
2026-07-22 11:10:38 -0500

    scene data changed.

---
+++ Castle/SiegeEngine/Core/Definitions/SceneSettings.cs
@@ -0,0 +1,22 @@
+﻿// Folder: SiegeEngine/Core/Definitions
+// File: SceneSettings.cs
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SiegeEngine.Core.Definitions
+{
+    public class SceneSettings
+    {
+        [JsonPropertyName("avatarPackKey")]
+        public string AvatarPackKey { get; set; }
+
+        [JsonPropertyName("controllerTypeName")]
+        public string ControllerTypeName { get; set; }
+
+        [JsonPropertyName("preferredSpawnPointIds")]
+        public List<int> PreferredSpawnPointIds { get; set; } = new List<int>();
+
+        [JsonPropertyName("cameraMode")]
+        public string CameraMode { get; set; }
+    }
+}
